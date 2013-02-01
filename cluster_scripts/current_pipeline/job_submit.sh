26f2be47e0ae2911ebc34ae33994c725b64f4e6c
kotoroshinoto <kotoroshinoto@e197d821-4802-406b-b52a-1082a7ddc3e2>
2013-02-01 01:12:05 +0000

    added seconds to timestamps

--- cluster_scripts/current_pipeline/job_submit.sh
+++ cluster_scripts/current_pipeline/job_submit.sh
@@ -5,6 +5,6 @@ JOBSCRIPT=$1
 shift
 JOBNAME=$1
 shift
-DATE=`date +"%Y-%m-%d-%H-%M"`
+DATE=`date +"%Y-%m-%d-%H-%M-%S"`
 #
 qsub -l h_vmem=40G -cwd -N $JOBNAME -o ./qsub_logs/$JOBNAME.$DATE.out -e ./qsub_logs/$JOBNAME.$DATE.err $HANDLER_SCRIPT -- $JOBSCRIPT $@
