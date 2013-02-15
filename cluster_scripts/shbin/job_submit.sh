72dd8498b23d1f8a74e5a35c64da09e10d5635bc
kotoroshinoto <kotoroshinoto@e197d821-4802-406b-b52a-1082a7ddc3e2>
2013-02-15 22:51:15 +0000

    made some changes on the cluster-side

--- cluster_scripts/shbin/job_submit.sh
+++ cluster_scripts/shbin/job_submit.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -x
 
-HANDLER_SCRIPT=/UCHC/HPC/Everson_HPC/cluster_scripts/shbin/run_qsub.sh
+HANDLER_SCRIPT=/UCHC/HPC/Everson_HPC/cluster_scripts/shbin/bouncer.sh
 JOBNAME=$1
 shift
 JOBSCRIPT=$1
 shift
 DATE=`date +"%Y-%m-%d-%H-%M-%S"`
 #
-qsub -l h_vmem=50G -cwd -N $JOBNAME -o ./qsub_logs/$JOBNAME.$DATE.out -e ./qsub_logs/$JOBNAME.$DATE.err $HANDLER_SCRIPT -- $JOBSCRIPT $@
+qsub -S /bin/bash -l h_vmem=50G -cwd -N $JOBNAME -o ./qsub_logs/$JOBNAME.$DATE.out -e ./qsub_logs/$JOBNAME.$DATE.err $HANDLER_SCRIPT $JOBSCRIPT $@
--- cluster_scripts/shbin/run_qsub.sh
+++ cluster_scripts/shbin/run_qsub.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -x
+echo "about to source from modules.sh"
 source /etc/profile.d/modules.sh
+echo "source complete"
+echo "about to load EversonLabBiotools/1.0 module"
 module load /UCHC/HPC/Everson_HPC/cluster_scripts/modulefiles/EversonLabBiotools/1.0
-echo "running: \"$@\""
+echo "module loaded"
+#echo "running: \"$@\""
 $@
