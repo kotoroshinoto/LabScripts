dd89dad47c60196dcd60c3e6fe5b3f9969cef226
mgooch <mgooch@Everson-HP-xw8600-Workstation>
2013-04-30 13:52:03 -0400

    Splitting & Editing Filter Scripts

---
+++ cluster_scripts/shbin/PipelineFilterBAM.sh
@@ -0,0 +1,106 @@
+source /UCHC/HPC/Everson_HPC/cluster_scripts/shbin/ScriptSettings.lib.sh
+#source ScriptSettings.lib.sh
+
+GROUPLBL=$1
+shift
+
+function filterRegions {
+
+mkdir -p filters
+
+mkdir -p filters/tmp
+
+SJM_JOB Filter1_BEDtools_$SAMPLE $GENERIC_JOB_RAM PipelineFilterBedtoolsCMD1.sh $1 $TARGET_BED ./filters/$1.bedfiltered.bam
+
+SJM_JOB_AFTER Filter1_BEDtools_$SAMPLE Prep7B_Realign_$SAMPLE
+
+SJM_JOB Filter2_SAMtools_$SAMPLE $GENERIC_JOB_RAM samtools view -bh -f 0x3 -F 0x60C -q $MAPQUAL -o ./filters/$1.samtools_filtered.bam ./filters/$1.bedfiltered.bam
+
+SJM_JOB_AFTER Filter2_SAMtools_$SAMPLE Filter1_BEDtools_$SAMPLE
+
+SJM_JOB Filter3_RMDuplicates_$SAMPLE $JAVA_JOB_RAM java -Xmx$JAVA_RAM -Xms$JAVA_RAM -Djava.io.tmpdir=$CURDIR/GATK_prep/tmp \
+-jar /UCHC/HPC/Everson_HPC/picard/bin/MarkDuplicates.jar \
+TMP_DIR=$CURDIR/filters/tmp \
+MAX_RECORDS_IN_RAM=$MRECORDS \
+I=./filters/$1.samtools_filtered.bam \
+O=./filters/$1.rmduplicates.bam \
+M=./filters/$1.dupmetrics \
+REMOVE_DUPLICATES=true \
+AS=true
+
+SJM_JOB_AFTER Filter3_RMDuplicates_$SAMPLE Filter2_SAMtools_$SAMPLE
+
+SJM_JOB Filter4_SORT_$SAMPLE $JAVA_JOB_RAM java -Xmx$JAVA_RAM -Xms$JAVA_RAM -Djava.io.tmpdir=$CURDIR/GATK_prep/tmp \
+-jar /UCHC/HPC/Everson_HPC/picard/bin/SortSam.jar \
+TMP_DIR=$CURDIR/filters/tmp \
+MAX_RECORDS_IN_RAM=$MRECORDS \
+I=./filters/$1.rmduplicates.bam \
+O=$2 \
+SORT_ORDER=coordinate
+#rm ./filters/$1.*
+
+SJM_JOB_AFTER Filter4_SORT_$SAMPLE Filter3_RMDuplicates_$SAMPLE
+}
+
+function getStats {
+#1 filename
+#2 RAM
+#3 curdir
+#4 mrecords
+#5 genome
+	SJM_JOB $2_GetStats_$SAMPLE $JAVA_JOB_RAM PipelineGetStats.sh $1 $JAVA_RAM $CURDIR $MRECORDS $GENOME
+}
+
+function Prepare_N_Filter_per_file {
+	mkdir -p sjm_logs
+SAMPLE=$1
+	#echo Prepare_N_Filter_per_file
+	#echo $1
+	#echo $2
+	#echo $3
+	#echo $4
+	#echo $5
+	#echo $6
+#Step2: 
+#	clean/Reorder/fix/add-replace-read-groups/sort
+SJM_FILE=./Step2-6.$1.sjm
+rm -f $SJM_FILE
+touch $SJM_FILE
+
+prepare4GATK $1.bam $1.4GATK.bam $2 $3 $4 $5 $6
+
+#Step3:
+#	samtools index
+#	Post alignment summary statistics with Picard/samtools flagstat and idxstat
+#	Picard GC bias metrics
+
+getStats $1.4GATK.bam PreFiltered
+SJM_JOB_AFTER PreFiltered_GetStats_$SAMPLE Prep5_MarkDuplicates_$SAMPLE
+
+#Step4: 
+#	GATK BaseRecalibration and the analyze covariates before and after
+#	GATK indelRealignment
+
+recalibrateBaseQual $1.4GATK.bam $1.4GATK.recal.bam
+indelrealign $1.4GATK.recal.bam $1.4GATK.recal.realn.bam
+
+#Step5:
+#	samtools view filter (Map Q 40, remove unmapped, keep mapped in proper pair, keep meeting vendor QC requirement)
+#	picard duplicate filter
+#	Bedtools intersectBed region filter 
+
+filterRegions $1.4GATK.recal.realn.bam $1.4GATK.recal.realn.filtered.bam
+
+#Step6: (repeat step 3 on filtered files)
+getStats $1.4GATK.recal.realn.filtered.bam PostFiltered
+SJM_JOB_AFTER PostFiltered_GetStats_$SAMPLE Filter4_SORT_$SAMPLE
+
+echo "log_dir $CURDIR/sjm_logs" >> $SJM_FILE
+}
+
+for var in "$@"
+do
+	ARGS=$(echo $var | tr "," "\n")
+	echo "ARGS: $ARGS"
+    Prepare_N_Filter_per_file $ARGS
+done
