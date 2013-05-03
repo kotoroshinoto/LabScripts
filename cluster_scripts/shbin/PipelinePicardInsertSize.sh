34254f82f18f598a62b1bcc996cfd60cfc6e52e6
mgooch <mgooch@Everson-HP-xw8600-Workstation>
2013-05-03 17:44:48 -0400

    herp

--- cluster_scripts/shbin/PipelinePicardInsertSize.sh
+++ cluster_scripts/shbin/PipelinePicardInsertSize.sh
@@ -5,8 +5,8 @@ GROUPLBL=$1
 shift
 SJM_FILE=./InsertSize.sjm
 function InsertSizePerSample {
-	SJM_JOB INS_SIZE_PRE_$FNAME $JAVA_JOB_RAM "java -jar /UCHC/HPC/Everson_HPC/picard/bin/CollectInsertSizeMetrics.jar I=$1.bam O=$1.pre.insertsize.out H=$1.pre.insertsize.histo R=$GENOME"
-	SJM_JOB INS_SIZE_POST_$FNAME $JAVA_JOB_RAM "java -jar /UCHC/HPC/Everson_HPC/picard/bin/CollectInsertSizeMetrics.jar I=$1.4GATK.recal.realn.filtered.bam O=$1.post.insertsize.out H=$1.post.insertsize.histo R=$GENOME"
+	SJM_JOB INS_SIZE_PRE_$1 $JAVA_JOB_RAM "java -jar /UCHC/HPC/Everson_HPC/picard/bin/CollectInsertSizeMetrics.jar I=$1.bam O=$1.pre.insertsize.out H=$1.pre.insertsize.histo R=$GENOME"
+	SJM_JOB INS_SIZE_POST_$1 $JAVA_JOB_RAM "java -jar /UCHC/HPC/Everson_HPC/picard/bin/CollectInsertSizeMetrics.jar I=$1.4GATK.recal.realn.filtered.bam O=$1.post.insertsize.out H=$1.post.insertsize.histo R=$GENOME"
 }
 
 for var in "$@"
