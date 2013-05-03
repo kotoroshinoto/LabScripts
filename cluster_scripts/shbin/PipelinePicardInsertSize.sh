a608114259039a5e3e917abf8d95453264a07a37
mgooch <mgooch@Everson-HP-xw8600-Workstation>
2013-05-03 17:48:14 -0400

    fixed insert size...again

--- cluster_scripts/shbin/PipelinePicardInsertSize.sh
+++ cluster_scripts/shbin/PipelinePicardInsertSize.sh
@@ -4,6 +4,7 @@ source /UCHC/HPC/Everson_HPC/cluster_scripts/shbin/ScriptSettings.lib.sh
 GROUPLBL=$1
 shift
 SJM_FILE=./InsertSize.sjm
+rm $SJM_FILE
 function InsertSizePerSample {
 	SJM_JOB INS_SIZE_PRE_$1 $JAVA_JOB_RAM "java -jar /UCHC/HPC/Everson_HPC/picard/bin/CollectInsertSizeMetrics.jar I=$1.bam O=$1.pre.insertsize.out H=$1.pre.insertsize.histo R=$GENOME"
 	SJM_JOB INS_SIZE_POST_$1 $JAVA_JOB_RAM "java -jar /UCHC/HPC/Everson_HPC/picard/bin/CollectInsertSizeMetrics.jar I=$1.4GATK.recal.realn.filtered.bam O=$1.post.insertsize.out H=$1.post.insertsize.histo R=$GENOME"
