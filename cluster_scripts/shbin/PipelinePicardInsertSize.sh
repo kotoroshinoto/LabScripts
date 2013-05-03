a85e7e7f505636876af24e3aa2a1a877a17572aa
mgooch <mgooch@Everson-HP-xw8600-Workstation>
2013-05-03 16:28:43 -0400

    insertsize commands added

--- cluster_scripts/shbin/PipelinePicardInsertSize.sh
+++ cluster_scripts/shbin/PipelinePicardInsertSize.sh
@@ -3,9 +3,10 @@ source /UCHC/HPC/Everson_HPC/cluster_scripts/shbin/ScriptSettings.lib.sh
 
 GROUPLBL=$1
 shift
-
+SJM_FILE=./InsertSize.sjm
 function InsertSizePerSample {
-	
+	SJM_JOB INS_SIZE_PRE_$FNAME $JAVA_JOB_RAM "java -jar /UCHC/HPC/Everson_HPC/picard/bin/CollectInsertSizeMetrics.jar I=$1.bam O=$1.pre.insertsize.out H=$1.pre.insertsize.histo R=$GENOME"
+	SJM_JOB INS_SIZE_POST_$FNAME $JAVA_JOB_RAM "java -jar /UCHC/HPC/Everson_HPC/picard/bin/CollectInsertSizeMetrics.jar I=$1.4GATK.recal.realn.filtered.bam O=$1.post.insertsize.out H=$1.post.insertsize.histo R=$GENOME"
 }
 
 for var in "$@"
