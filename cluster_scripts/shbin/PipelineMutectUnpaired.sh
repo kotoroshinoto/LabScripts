c22929c232ccb8c7a22feb8132c85623b2439d7e
gooch <gooch@Gooch-Serval-Professional>
2013-05-20 13:08:07 -0400

    changed unpaired mutect script, requires testing to verify functionality of commands

--- cluster_scripts/shbin/PipelineMutectUnpaired.sh
+++ cluster_scripts/shbin/PipelineMutectUnpaired.sh
@@ -1,18 +1,19 @@
 #!/bin/bash -x
 source /UCHC/HPC/Everson_HPC/cluster_scripts/shbin/ScriptSettings.lib.sh
 #source ScriptSettings.lib.sh
+GROUPLBL=$1
+#echo "group label: $GROUPLBL" 
+shift
 
-NC=$1.4GATK.recal.realn.filtered.bam
-NF=$2.4GATK.recal.realn.filtered.bam
-TC=$3.4GATK.recal.realn.filtered.bam
-TF=$4.4GATK.recal.realn.filtered.bam
-GROUPLBL=$5
 SJM_FILE=./RunMutect.sjm
+
+#NC=$1.4GATK.recal.realn.filtered.bam
+#NF=$2.4GATK.recal.realn.filtered.bam
+#TC=$3.4GATK.recal.realn.filtered.bam
+#TF=$4.4GATK.recal.realn.filtered.bam
+
 function runMutect {
-	LBLN=$3
-	LBLT=$4
-	FNAME=$3_$4
-	SJM_JOB MUTECT_$FNAME $JAVA_JOB_RAM "mutect -R $GENOME --cosmic $COSMIC --dbsnp $DBSNP --intervals $TARGET_BED --input_file:normal $1 --input_file:tumor $2 --out mutect_call_stats.$FNAME.txt --coverage_file mutect_coverage.$FNAME.wig.txt --vcf mutect.$FNAME.vcf --tumor_sample_name $LBLT --normal_sample_name $LBLN --enable_extended_output"
+	SJM_JOB MUTECT_$1 $JAVA_JOB_RAM "mutect -R $GENOME --cosmic $COSMIC --dbsnp $DBSNP --intervals $TARGET_BED --input_file:tumor $2 --out mutect_call_stats.$1.txt --coverage_file mutect_coverage.$1.wig.txt --vcf mutect.$1.vcf --tumor_sample_name $1 --enable_extended_output"
 }
 
 function runSJMfile {
@@ -30,5 +31,11 @@ runMutect $NC $NF NC NF
 runMutect $NF $NC NF NC
 runMutect $TC $TF TC TF
 
+for var in "$@"
+do
+	#ARGS=$(echo $var | tr "," " ")
+	#echo "ARGS: $ARGS"
+    runMutect $var $var.4GATK.recal.realn.filtered.bam
+done
 mkdir -p sjm_logs
 echo "log_dir $CURDIR/sjm_logs" >> $SJM_FILE
