73c2381db677b8703c0f46480c0d15bb3406ae27
Michael Gooch <mgooch@kotoro.com>
2013-06-12 06:01:14 -0400

    removed echo & extra whitespace

--- cluster_scripts/shbin/PipelineGATK_genotyper.sh
+++ cluster_scripts/shbin/PipelineGATK_genotyper.sh
@@ -7,15 +7,15 @@ shift
 
 function Genotype_per_file {
 	SJM_JOB $2_GATK_GENOTYPE $JAVA_JOB_RAM "GATK \
-	-T UnifiedGenotyper \
-	-R $GENOME \
-	-I $1 \
-	--dbsnp $DBSNP \
-	-o $1.snps.dcov$3.raw.vcf \
-	-stand_call_conf 50.0 \
-	-stand_emit_conf 10.0 \
-	-dcov $3 \
-	-L $TARGET_BED"
+-T UnifiedGenotyper \
+-R $GENOME \
+-I $1 \
+--dbsnp $DBSNP \
+-o $1.snps.dcov$3.raw.vcf \
+-stand_call_conf 50.0 \
+-stand_emit_conf 10.0 \
+-dcov $3 \
+-L $TARGET_BED"
 	#dcov [50 for 4x, 200 for >30x WGS or Whole exome]
 }
 
@@ -24,6 +24,6 @@ SJM_RESET
 for var in "$@"
 do
 	ARGS=$(echo $var | tr "," "\n")
-	echo "ARGS: $ARGS"
+	#echo "ARGS: $ARGS"
     Genotype_per_file $ARGS
 done
