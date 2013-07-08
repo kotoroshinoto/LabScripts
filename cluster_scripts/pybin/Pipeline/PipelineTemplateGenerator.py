f4d3f8acf70ed521cedd04c1161186d902f71066
Michael Gooch <mgooch@kotoro.com>
2013-07-08 18:33:04 -0400

    test

--- cluster_scripts/pybin/Pipeline/PipelineTemplateGenerator.py
+++ cluster_scripts/pybin/Pipeline/PipelineTemplateGenerator.py
@@ -110,6 +110,7 @@ def parseVars(template,Vars):
         PipelineError("[PipelineTemplateGenerator.parseVars] template object is None");
     if Vars is None:
         PipelineError("[PipelineTemplateGenerator.parseVars] No variables provided");
+    print(Vars)
     for Var in Vars:
         eqsplit=Var.split("=")
         if (len(eqsplit)!=2):
