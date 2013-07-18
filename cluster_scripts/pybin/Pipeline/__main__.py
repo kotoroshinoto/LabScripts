91c1e3ffeb011de9ea09a52fa754893c5d582576
Michael Gooch <mgooch@kotoro.com>
2013-07-18 12:12:51 -0400

    missing an endline

--- cluster_scripts/pybin/Pipeline/__main__.py
+++ cluster_scripts/pybin/Pipeline/__main__.py
@@ -5,5 +5,5 @@ Created on Mar 18, 2013
 '''
 import sys
 if __name__ == '__main__':
-    sys.stderr.write("This is currently a placeholder, it will eventually be implemented to forward commandline args to the appropriate python module")
+    sys.stderr.write("This is currently a placeholder, it will eventually be implemented to forward commandline args to the appropriate python module\n")
     pass
