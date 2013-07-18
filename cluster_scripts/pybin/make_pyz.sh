3479dea84bc8379de7bf97beeba252fe08309499
Michael Gooch <mgooch@kotoro.com>
2013-07-18 12:35:18 -0400

    moved something

--- cluster_scripts/pybin/make_pyz.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-rm Pipeline.zip
-cd Pipeline
-zip -r ../Pipeline.zip ./*
-cd ..
-cat shebang.txt Pipeline.zip >Pipeline.pyz
-chmod +x Pipeline.pyz
---
+++ cluster_scripts/pybin/pyzip_files/make_pyz.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd /UCHC/HPC/Everson_HPC/cluster_scripts/pybin
+rm Pipeline.zip
+zip -r Pipeline.zip Pipeline
+cd pyzip_files
+zip ../Pipeline.zip __main__.py
+cd ..
+cat pyzip_files/shebang.txt Pipeline.zip >Pipeline.pyz
+chmod +x Pipeline.pyz
+rm Pipeline.zip
