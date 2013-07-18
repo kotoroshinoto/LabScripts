640a4049979e119a0f6a88a172303f26b98cd598
Michael Gooch <mgooch@kotoro.com>
2013-07-18 12:30:20 -0400

    more organizing

--- cluster_scripts/pybin/Pipeline/__main__.py
+++
@@ -1,9 +0,0 @@
-'''
-Created on Mar 18, 2013
-
-@author: Gooch
-'''
-import sys
-if __name__ == '__main__':
-    sys.stderr.write("This is currently a placeholder, it will eventually be implemented to forward commandline args to the appropriate python module\n")
-    pass
---
+++ cluster_scripts/pybin/pyzip_files/__main__.py
@@ -0,0 +1,19 @@
+'''
+Created on Mar 18, 2013
+
+@author: Gooch
+'''
+import sys
+if 'Pipeline.pyz' in sys.argv[0]:
+    sys.path.append('/UCHC/HPC/Everson_HPC/cluster_scripts/pybin/Pipeline.pyz')
+import Pipeline.commands.PipelineTemplateGenerator
+import Pipeline.commands.PipelineGenerateSJM
+def main():
+    sys.stderr.write("This is currently a placeholder, it will eventually be implemented to forward commandline args to the appropriate python module\n")
+    sys.stderr.write("args: '%s'" % (' '.join(sys.argv)))
+    if (sys.argv[1].lower() == "maketemplate"):
+        Pipeline.commands.PipelineTemplateGenerator.main(sys.argv)
+    return 0
+if __name__ == '__main__':
+    sys.exit(main())
+    
