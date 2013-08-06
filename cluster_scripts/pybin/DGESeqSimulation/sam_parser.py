646b2f8e48c6db4da2bb96984acfd4044eda5470
Bing <Bing@10.22.10.74>
2013-08-06 13:29:08 -0400

    Fix syntax

--- cluster_scripts/pybin/DGESeqSimulation/sam_parser.py
+++ cluster_scripts/pybin/DGESeqSimulation/sam_parser.py
@@ -80,8 +80,9 @@ def processSAMFile(sam_filename, gtf_list):
     print('Reading...')
     for seqread in seqinput.fetch():
         readcount += 1
-        transcripts_at_chromosome = gtf_list[seqinput.getrname(seqread.tid)]
-        print ("reference name: %s" (seqinput.getrname(seqread.tid)))
+        contig=seqinput.getrname(seqread.tid)
+        print ("reference name: %s" (contig))
+        transcripts_at_chromosome = gtf_list[contig]
         '''
         There is no need to use indices here,
         since you dont use them for anything other than accessing the list, 
