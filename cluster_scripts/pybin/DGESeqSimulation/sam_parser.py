e24f99ef30daf288fce222acb87710726358b189
Bing <Bing@10.22.10.74>
2013-08-06 16:04:16 -0400

    Clean comments

--- cluster_scripts/pybin/DGESeqSimulation/sam_parser.py
+++ cluster_scripts/pybin/DGESeqSimulation/sam_parser.py
@@ -6,14 +6,14 @@ version 2013.08.05
 takes input from gene list file and sequence alignment map file and compares sequence reads to calculated stable regions of transcripts
 
 run location: ssh mgooch@sig2-glx.cam.uchc.edu
-sample command: samtools view /UCHC/Everson/Projects/Bladder/Pt5/RNA/TSRNA091711TCBL5_TOPHAT/accepted_hits.bam | python /UCHC/HPC/Everson_HPC/LabScripts/cluster_scripts/pybin/DGESeqSimulation/sam_parser.py /dev/stdin Pt5_TC_results.txt 200
+sample command: python /UCHC/HPC/Everson_HPC/LabScripts/cluster_scripts/pybin/DGESeqSimulation/sam_parser.py /UCHC/Everson/Projects/Bladder/Pt5/RNA/TSRNA091711TCBL5_TOPHAT/accepted_hits.bam pysam_Pt5_TC_results.txt 200
 
 """
 import os, sys, pickle, datetime
 import gtf_reader as greader
 import pysam
 
-is_debug = True
+is_debug = False
 
 def inputTranscriptList(gtf_filename, transcript_list_filename, simulation_length):
     """reads existing transcript list or generates new list if needed from GTF file"""
@@ -35,19 +35,13 @@ def processSAMFile(sam_filename, gtf_list):
     seqinput = pysam.Samfile(sam_filename)#automatically checks for 'rb' and then 'r' modes
     # read SAM file up to limit and run comparisons to transcript list
     readcount = 0
-    if is_debug is True:
+    if is_debug:
         readlimit = 10000 # debugging
     print('Reading...')
     for seqread in seqinput.fetch(until_eof=True):
         readcount += 1
         transcripts_at_chromosome = gtf_list[seqinput.getrname(seqread.tid)]
-        '''
-        There is no need to use indices here,
-        since you dont use them for anything other than accessing the list, 
-        so I modified to show you pythonic method
-        '''
         for transcript in transcripts_at_chromosome:
-#            print(transcript)
             '''
             TODO:
             could possibly iterate over transcripts and use 
@@ -61,31 +55,16 @@ def processSAMFile(sam_filename, gtf_list):
             or on first fetch (and then retains it for later use)
             '''
             read_end = seqread.pos+(seqread.qend-seqread.qstart)
-#            print ("read start: %d, read end %d" %(seqread.pos,read_end))
             if transcript.start <= read_end and transcript.end >= seqread.pos:
-                #transcript.read_names.append(seqread.read_name)
-                #transcript.read_quality.append(seqread.read_quality)
                 transcript.expression_count += 1
                 #if is_debug is True:
                     #print('Found match on line %d for %r' % (readcount, transcript.name)) #debugging
-        '''
-        there should not be any reason to store this back again, 
-        its already stored in gtf_list, data type is mutable,
-        all changes should already be reflected in original location
-        '''
-        #gtf_list[seqread.chromosome] = transcripts_at_chromosome
-        if readcount % 10000 == 0:
+        if readcount % 100000 == 0:
             print('Processed %d reads @ %s' % (readcount,datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
-        if is_debug is True:
+        if is_debug:
             if readcount == readlimit:
                 break
     seqinput.close()
-    '''
-    don't need to return lists, 
-    since they're mutable, 
-    original list should reflect all modifications
-    return gtf_list
-    '''
 def outputMatches(output_filename, gtf_list):
     print('Writing...')
     
@@ -93,16 +72,13 @@ def outputMatches(output_filename, gtf_list):
     os.chdir(os.path.join(os.path.dirname(__file__), 'Output'))
     output = open(output_filename, 'w')
     output.write('Transcript Name\tNumber of Exons\tNumber of Expressions\tTranscript Number ID\n')
-    try:
-        len(gtf_list)
-    except IndexError:
-        print('Cannot output an empty table')
     for chromosome in gtf_list:
         transcripts_at_chromosome = gtf_list[chromosome]
         for transcript in transcripts_at_chromosome:
             if transcript.expression_count > 0:
                 output_string = '%s\t%d\t%d\t%d' % (transcript.name, transcript.num_exons, transcript.expression_count, transcript.num_id)
-                output.write(output_string + '\n')
+                if is_debug:
+                    output.write(output_string + '\n')
                 print(output_string)
     output.close()
     os.chdir(old_dir)
@@ -111,12 +87,12 @@ def outputMatches(output_filename, gtf_list):
 # define command line argument input
 if len(sys.argv) != 4:
     sys.stderr.write("\nScript must be given 3 arguments: input filename, output filename, and simulation sequence length")
-input_file = sys.argv[1] # when using from samtools view: samtools view filename.bam | sam_parser.py /dev/stdin output_filename sim_sequence_length
+input_file = sys.argv[1]
 output_file = sys.argv[2]
 simulation_length = int(sys.argv[3])
 
 print('\nStarting the script...')
 gtf_list = inputTranscriptList('genes.gtf', output_file[:-11] + 'simlength' + str(simulation_length) + '_transcripts.csv', simulation_length)
-processSAMFile(input_file, gtf_list)#reflecting fact that gtf_list is modified, no reason to return/store it again
+processSAMFile(input_file, gtf_list)
 outputMatches(output_file, gtf_list)
 print('Job is Finished!')
