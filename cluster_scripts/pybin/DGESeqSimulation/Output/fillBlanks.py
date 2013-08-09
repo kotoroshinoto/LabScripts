bc28b617e7a07054ec70bccd0eba7f177bfe88ab
Bing <Bing@10.22.10.74>
2013-08-09 17:59:27 -0400

    Debug

--- cluster_scripts/pybin/DGESeqSimulation/Output/fillBlanks.py
+++ cluster_scripts/pybin/DGESeqSimulation/Output/fillBlanks.py
@@ -10,8 +10,8 @@ data=[]
 if len(sys.argv) <= 1:
     sys.stderr.write('Error: please input at least two files\n')
 print('Generating file databases...')
-for item in sys.argv:
-    files.append(item)
+for x in range(1, len(sys.argv)):
+    files.append(sys.argv[x])
     data.append(dict())
 
 print('Examining files...')
@@ -19,28 +19,31 @@ for x in range(0, len(files)):
     inputfile=open(files[x],"r")
     for line in inputfile:
         splitline=line.split("\t")
-        for y in range(0, len(data)):
-            if x == y: # data corresponds to file
-                transcript=[]
-                transcript.append(splitline[0])
-                try:
-                    transcript.append(splitline[2])
-                except: #some lines are not formated with 4 columns
-                    continue
-                data[x][splitline[0]]=transcript
-            else:#data is from another file, add missing entries
-                if not splitline[0] in data:
+        if len(splitline) == 4:
+            for y in range(0, len(data)):
+                if x == y: # data corresponds to file
                     transcript=[]
                     transcript.append(splitline[0])
-                    transcript.append('0')
-                    #TODO add values like zeroes or blanks to appropriate columns
+                    transcript.append(splitline[2])
                     data[y][splitline[0]]=transcript
+                else:#data is from another file, add missing entries
+                    if splitline[0] not in data[y]:
+                        transcript=[]
+                        transcript.append(splitline[0])
+                        transcript.append('0')
+                        #TODO add values like zeroes or blanks to appropriate columns
+                        data[y][splitline[0]]=transcript
     inputfile.close()
 
 print('Writing results...')
 for x in range(0, len(files)):
     outputfilename=files[x]
     outputfile=open("merged_" + outputfilename, "w")
-    for geneLabel in data[x].keys().sort():
+    geneLabels=[]
+    for item in data[x].keys():
+        geneLabels.append(item)
+    geneLabels.sort()
+    geneLabels.reverse()
+    for geneLabel in geneLabels:
         outputfile.write("\t".join(data[x][geneLabel]) + "\n")
     outputfile.close()
