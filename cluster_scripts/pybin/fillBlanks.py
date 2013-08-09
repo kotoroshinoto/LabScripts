b4bf9813ae6427255e168e6f35f69d092ee6d484
Bing <Bing@10.22.10.74>
2013-08-09 16:22:00 -0400

    Add new script that compensates for transcripts that only exist in one sample

--- cluster_scripts/pybin/fillBlanks.py
+++ cluster_scripts/pybin/fillBlanks.py
@@ -3,10 +3,12 @@ Created on Aug 9, 2013
 
 @author: mgooch
 '''
-import sys,os
+import sys
 
 files=[]
 data=[]
+if len(sys.argv) <= 1:
+    sys.stderr.write('Error: please input at least two files\n')
 for item in sys.argv:
     files.append(item)
     data.append(dict())
@@ -17,16 +19,22 @@ for x in range(0, len(files)):
         splitline=line.split("\t")
         for y in range(0, len(data)):
             if x == y: # data corresponds to file
-                data[x][splitline[0]]=splitline
+                transcript=[]
+                transcript.append(splitline[0])
+                transcript.append(splitline[2])
+                data[x][splitline[0]]=transcript
             else:#data is from another file, add missings entries
                 if not data.has_key(splitline[0]):
-                    data[y][splitline[0]]=[]
-                    data[y][splitline[0]].append(splitline[0])
+                    transcript=[]
+                    transcript.append(splitline[0])
+                    transcript.append('0')
                     #TODO add values like zeroes or blanks to appropriate columns
+                    data[y][splitline[0]]=transcript
     inputfile.close()
 
-for item in data:
-    for geneLabel in item.keys().sorted():
-        outputfile=open("outputfile","w")
-        
-        outputfile.close()
+for x in range(0, len(files)):
+    outputfilename=files[x]
+    outputfile=open("merged_" + outputfilename, "w")
+    for geneLabel in data[x].keys().sorted():
+        outputfile.write("\t".join(data[x][geneLabel]) + "\n")
+    outputfile.close()
