e6c39561cfd6a33f66caf797b7dcb5a14fa9d2a8
Michael Gooch <mgooch@kotoro.com>
2013-09-05 09:32:04 -0400

    maf line processing method completed

--- cluster_scripts/cgi-bin/TCGA_maf_counter.pl
+++ cluster_scripts/cgi-bin/TCGA_maf_counter.pl
@@ -77,7 +77,51 @@ sub new{
 	return bless $self, $class;
 }
 sub processline{
-	#TODO finish this
+	my ($class,@params)= @_;
+	die "used as an object method" if ref $class;
+	if (scalar(@params) != 1){die "method takes 1 and only 1 argument";}
+	my @columns=split('\t',$params[0]);
+	if (scalar(@columns) != 37){die "line does not have correct # of columns (37)"}
+	my $newobj=$class->new();
+	$newobj->{Hugo_Symbol}=$columns[0];
+	$newobj->{Entrez_Gene_Id}=$columns[1];
+	$newobj->{Center}=$columns[2];
+	$newobj->{Ncbi_Build}=$columns[3];
+	$newobj->{Chrom}=$columns[4];
+	$newobj->{Start_Position}=$columns[5];
+	$newobj->{End_Position}=$columns[6];
+	$newobj->{Strand}=$columns[7];
+	$newobj->{Variant_Classification}=$columns[8];
+	$newobj->{Variant_Type}=$columns[9];
+	$newobj->{Reference_Allele}=$columns[10];
+	$newobj->{Tumor_Seq_Allele1}=$columns[11];
+	$newobj->{Tumor_Seq_Allele2}=$columns[12];
+	$newobj->{Dbsnp_Rs}=$columns[13];
+	$newobj->{Dbsnp_Val_Status}=$columns[14];
+	$newobj->{Tumor_Sample_Barcode}=$columns[15];
+	$newobj->{Matched_Norm_Sample_Barcode}=$columns[16];
+	$newobj->{Match_Norm_Seq_Allele1}=$columns[17];
+	$newobj->{Match_Norm_Seq_Allele2}=$columns[18];
+	$newobj->{Tumor_Validation_Allele1}=$columns[19];
+	$newobj->{Tumor_Validation_Allele2}=$columns[20];
+	$newobj->{Match_Norm_Validation_Allele1}=$columns[21];
+	$newobj->{Match_Norm_Validation_Allele2}=$columns[22];
+	$newobj->{Verification_Status}=$columns[23];
+	$newobj->{Validation_Status}=$columns[24];
+	$newobj->{Mutation_Status}=$columns[25];
+	$newobj->{Sequencing_Phase}=$columns[26];
+	$newobj->{Sequence_Source}=$columns[27];
+	$newobj->{Validation_Method}=$columns[28];
+	$newobj->{Score}=$columns[29];
+	$newobj->{Bam_File}=$columns[30];
+	$newobj->{Sequencer}=$columns[31];
+	$newobj->{Tumor_Sample_UUID}=$columns[32];
+	$newobj->{Matched_Norm_Sample_UUID}=$columns[33];
+	$newobj->{File_Name}=$columns[34];
+	$newobj->{Archive_Name}=$columns[35];
+	$newobj->{Line_Number}=$columns[36];
+	return $newobj;
+	
 }
 1;
 package FeatureCounter;
